0a243b6828cf847d31efc10e4c484a989e6066a7
hocgin <hocgin@gmail.com>
2021-09-15 23:30:52 +0800

    调整

--- README.md
+++ README.md
@@ -1,19 +1,40 @@
 # uniapp-starter
 
-## Project setup
-```
-yarn install
-```
+> - 这是一份基于 uni-app 模版, 如果有需要可以点击`Use this Template`进行使用。
+> - [uni-app](https://uniapp.dcloud.io/)
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+## 基础架构
 
-### Compiles and minifies for production
-```
-yarn build
-```
+- Vue
+- uni-app
+- uView
+- Sass
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## 目录结构
+
+```shell
+.
+├── README.md
+├── babel.config.js
+├── dist                   # 编译文件目录
+│   ├── build
+│   └── dev
+├── package-lock.json
+├── package.json            # 项目配置
+├── postcss.config.js       # CSS配置
+├── public                  # 页面模版
+│   └── index.html
+├── src                     # 源码
+│   ├── App.vue       # 应用入口
+│   ├── main.ts       # 程序入口
+│   ├── manifest.json # uniapp 相关配置
+│   ├── pages         # 页面
+│   ├── pages.json    # 页面配置
+│   ├── sfc.d.ts
+│   ├── static        # 静态文件
+│   ├── uni.scss      # 全局样式
+│   └── uview-ui      # uView 组件
+├── tsconfig.json
+├── typings.d.ts
+└── webpack.config.js
+```
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,50 +1,54 @@
 <template>
-	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
-	</view>
+  <view class="content">
+    <image class="logo" src="../../static/logo.png"></image>
+    <view>
+      <text class="title">{{ title }}</text>
+      <text class="title">Hello Word</text>
+      <u-button type="primary">主要按钮</u-button>
+      <u-rate :count="count" v-model="value"></u-rate>
+    </view>
+  </view>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-	export default Vue.extend({
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+import Vue from 'vue';
 
-		},
-		methods: {
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Hello',
+      count: 4,
+      value: 2
+    }
+  },
+  onLoad() {
 
-		}
-	});
+  },
+  methods: {}
+});
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style type="text/scss" lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+.logo {
+  height: "200rpx";
+  width: "200rpx";
+  margin: "200rpx" auto "50rpx" auto;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.text-area {
+  display: flex;
+  justify-content: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: "36rpx";
+  color: #8f8f94;
+}
 </style>
